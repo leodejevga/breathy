65301a8399160e6ac206ef77f6bf261fff554062
Jevgenij Huebert <huebertj@students.uni-marburg.de>
2017-02-21 16:20:26 +0100

    merge

--- Breathy/source/src/main/java/com/apps/philipps/source/SaveData.java
+++ Breathy/source/src/main/java/com/apps/philipps/source/SaveData.java
@@ -10,13 +10,23 @@ import java.io.OutputStream;
 /**
  * This Class must be instatiate from Backend Classes and save the specific data on the hard drive
  */
-public class SaveData {
+public class SaveData<T> {
+    private String fileName;
     //TODO: Diese Klasse soll von den Objekten Backend erstellt werden und sich darum kümmern bestimmte Daten auf die Festplatte zu speichern
 
-    public SaveData(){
+    public SaveData(String fileName){
+        this.fileName = fileName;
     }
 
-    public void write(){
+    public void write(T data){
 
+    }
+
+    public T read(){
+        return read(0);
+    }
+
+    public T read(int index){
+        return null;
     }
 }
