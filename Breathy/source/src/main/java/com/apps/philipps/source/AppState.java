db4bdd4020de866d3c8469747d85d0dd9971bd12
Jevgenij Huebert <huebertj@students.uni-marburg.de>
2017-02-22 20:04:04 +0100

    Build removed

---
+++ Breathy/source/src/main/java/com/apps/philipps/source/AppState.java
@@ -0,0 +1,9 @@
+package com.apps.philipps.source;
+
+/**
+ * Created by Jevgenij Huebert on 22.02.2017. Project Breathy
+ */
+public class AppState {
+    public static boolean inGame = false;
+    public static boolean recordData = false;
+}
