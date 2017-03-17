1ceec2f77e17ed438efb3c2566ae34de97cd36ac
Schmidtpott <Juergen.schmidtpott@gmx.de>
2017-03-17 18:08:55 +0100

    2D start

--- Breathy/audioSurf/src/main/java/com/apps/philipps/audiosurf/activities/Game2D.java
+++ Breathy/audioSurf/src/main/java/com/apps/philipps/audiosurf/activities/Game2D.java
@@ -1,14 +1,25 @@
 package com.apps.philipps.audiosurf.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
+import com.apps.philipps.audiosurf.Barricade;
+import com.apps.philipps.audiosurf.Player;
 import com.apps.philipps.audiosurf.R;
 import com.apps.philipps.source.AppState;
 import com.apps.philipps.source.BreathData;
+import com.apps.philipps.source.GameUtil;
 import com.apps.philipps.source.abstracts.AbstractGameActivity;
+
+import java.util.ArrayList;
 
 public class Game2D extends AbstractGameActivity {
 
@@ -16,9 +27,37 @@ public class Game2D extends AbstractGameActivity {
     TextView frameCount;
     TextView dataDisplay;
 
+    // display parameter
+    int displayWidth;
+    int displayHeight;
+    Canvas canvas;
+
+
+    // Bitmaps
+    Bitmap playerbmp = BitmapFactory.decodeResource(getResources(), R.drawable.ship);
+    Bitmap barricadebmp = BitmapFactory.decodeResource(getResources(), R.drawable.enemy);
+
+    // Game stats
+    Player player;
+    ArrayList<Barricade> barricades = new ArrayList<Barricade>();
+    double nextEmeny;
+    double timerNextEmeny;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //get display width and height
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        displayWidth = size.x;
+        displayHeight = size.y;
+
+        // ini Game
+        player = new Player(this, playerbmp, displayHeight-20, displayWidth/2 - 8);
+
         setContentView(R.layout.as_game2d);
         AppState.recordData = AppState.inGame = true;
     }
@@ -29,6 +68,17 @@ public class Game2D extends AbstractGameActivity {
         dataDisplay.setText(data);
         framerate.setText(frameRate + " fps");
         frameCount.setText("frame " + frame);
+        player.draw(canvas);
+        for(Barricade b: barricades){
+            b.draw(canvas);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        player.onTouch(event);
+
+        return false;
     }
 
     @Override
@@ -36,5 +86,20 @@ public class Game2D extends AbstractGameActivity {
         framerate = (TextView) findViewById(R.id.as_framerate);
         dataDisplay = (TextView) findViewById(R.id.as_data);
         frameCount = (TextView) findViewById(R.id.as_frameCount);
+    }
+
+    public void generateAIObjects(){
+        timerNextEmeny++;
+
+        if(timerNextEmeny>nextEmeny){
+            Barricade b = new Barricade(this, barricadebmp, 20, 0); //ToDo richtige Werte übergeben
+            barricades.add(b);
+            nextEmeny = GameUtil.triangularDistribution(20, 60, 40);
+            timerNextEmeny=0;
+        }
+    }
+
+    public int getDisplayWidth() {
+        return displayWidth;
     }
 }
