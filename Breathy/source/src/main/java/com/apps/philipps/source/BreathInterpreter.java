438b72a65d8fd933d691600943933db3b2891e70
jhuebert64p <huebertj@students.uni-marburg.de>
2017-03-29 22:11:29 +0200

    schön machen

--- Breathy/source/src/main/java/com/apps/philipps/source/BreathInterpreter.java
+++ Breathy/source/src/main/java/com/apps/philipps/source/BreathInterpreter.java
@@ -13,26 +13,28 @@ public class BreathInterpreter {
 
     private static List<IObserver> observer = new ArrayList<>();
 
-    public enum Status{
+    public enum Status {
         In,
         Out,
         None
     }
-    public static int strength=0;
+
+    public static int strength = 0;
     public static Status status = Status.None;
 
-    public static void addObserver(IObserver o){
+    public static void addObserver(IObserver o) {
         observer.add(o);
     }
-    public static boolean removeObserver(IObserver o){
+
+    public static boolean removeObserver(IObserver o) {
         return observer.remove(o);
     }
 
     public static void setStatus() {
         int norm = AppState.breathyNormState; //TODO: Der breathyNormState zeigt den Druck an, ohne, dass jemand reinpustet. Dieser soll benutzt werden um strength richtig zu identifizieren
-        Integer[] data = BreathData.get(0,10); //TODO: Diese Daten sollen analysiert werden und der status und strength entsprechend gesetzt werden.
+        Integer[] data = BreathData.get(0, 10); //TODO: Diese Daten sollen analysiert werden und der status und strength entsprechend gesetzt werden.
 
-        for(IObserver o : observer)
+        for (IObserver o : observer)
             o.call(status, strength);
     }
 
