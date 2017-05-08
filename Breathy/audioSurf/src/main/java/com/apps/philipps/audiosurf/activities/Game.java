a5c5e1fa987cd470ea0ea57596806dd982fee398
Duc <iwnfubb@googlemail.com>
2017-05-09 00:07:43 +0200

    #29 new features: -musik auswählen -musik wiedergeben

--- Breathy/audioSurf/src/main/java/com/apps/philipps/audiosurf/activities/Game.java
+++ Breathy/audioSurf/src/main/java/com/apps/philipps/audiosurf/activities/Game.java
@@ -1,7 +1,9 @@
 package com.apps.philipps.audiosurf.activities;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import com.apps.philipps.audiosurf.Backend;
 import com.apps.philipps.source.AppState;
 import com.apps.philipps.source.BreathData;
 import com.apps.philipps.source.helper._3D.Activity3D;
@@ -11,18 +13,21 @@ import com.apps.philipps.source.interfaces.IObserver;
  * Game Activity
  */
 public class Game extends Activity3D implements IObserver {
+    MediaPlayer myMediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppState.inGame = AppState.recordData = true;
         BreathData.addObserver(this);
-
+        myMediaPlayer = MediaPlayer.create(Game.this, Backend.getDefaut_music_resource_id());
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         AppState.inGame = AppState.recordData = false;
+        myMediaPlayer.release();
     }
 
 
@@ -30,4 +35,15 @@ public class Game extends Activity3D implements IObserver {
     public void call(Object... messages) {
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        myMediaPlayer.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myMediaPlayer.start();
+    }
 }
