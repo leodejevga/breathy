fc2474d4bf377c9d360a45feebe82725b0b3732f
Jevgenij Huebert <huebertj@students.uni-marburg.de>
2017-07-01 18:30:50 +0200

    first changes

--- Breathy/source/src/main/java/com/apps/philipps/source/BreathData.java
+++ Breathy/source/src/main/java/com/apps/philipps/source/BreathData.java
@@ -15,10 +15,9 @@ import java.util.List;
  * Created by Jevgenij Huebert on 29.01.2017. Project Breathy
  */
 public abstract class BreathData {
-    //TODO: Hier werden Bluetooth Daten gesammelt und live gefiltert, sodass sie wieder als Rückmeldung fungieren können
-    //Diese Klasse soll statisch agieren
+
     private static RAM ram;
-    private static int ramSize = 5;
+    private static int ramSize = 6;
     private static int blockSize = 3;
     private static boolean initialized = false;
 
@@ -77,14 +76,14 @@ public abstract class BreathData {
     public static void add(String value) {
         if (AppState.recordData) {
             for (int i : convert(value))
-                ram.add(i);
+                ram.addData(i);
         }
     }
 
     public static void add(int... values) {
         if (AppState.recordData) {
             for (int i : values)
-                ram.add(i);
+                ram.addData(i);
         }
     }
 
@@ -111,20 +110,24 @@ public abstract class BreathData {
      * @param range the range of values to get
      * @return the list of values at index with size of range
      */
-    public static Integer[] get(int index, int range) {
-        Integer[] result = new Integer[range];
-        for (int i = index; i < index + range; i++) {
-            result[i] = ram.get(i);
-        }
+    public static Element[] get(int index, int range) {
+        Element[] result = new Element[range];
+        for (int i = index; i < index + range; i++)
+            result[i - index] = ram.getData(i);
         return result;
     }
 
+    /**
+     * Get stream of data as String from index with the amount of range
+     * @param index start of stream
+     * @param range amount of elements
+     * @return stream of data as String
+     */
     public static String getAsString(int index, int range) {
         String result = "";
-        for (int i = index; i < index + range; i++) {
-            if (ram.get(i) != null)
-                result += ", " + ram.get(i);
-        }
+        Element[] elements = get(index, range);
+        for (Element element : elements)
+            result += ", " + element;
         return result.length() > 1 ? result.substring(2) : "";
     }
 
@@ -134,7 +137,7 @@ public abstract class BreathData {
      * @param index the index of the Value
      * @return the integer at index, null if IndexOutOfRange
      */
-    public static Integer get(int index) {
+    public static DataBlock get(int index) {
         return ram.get(index);
     }
 
@@ -143,7 +146,7 @@ public abstract class BreathData {
         /**
          * Instantiates a new Limited list.
          */
-        private SaveData<Object[]> saveData = null;
+        private SaveData<DataBlock> saveData = null;
 
         /**
          * Instantiates a new Limited list.
@@ -151,7 +154,6 @@ public abstract class BreathData {
         public RAM(Context context) {
             super();
             if (ramSize != 0) {
-                block = 0L;
                 saveData = new SaveData<>(context);
             }
         }
@@ -160,35 +162,50 @@ public abstract class BreathData {
         public boolean add(DataBlock t) {
             super.add(0, t);
 
-            Log.d("ram", t + "");
-            if (saveData != null && size() > ramSize) {
-                Object[] data = subList(size() - blockSize, size() - 1).toArray();
-                saveData.writeObject("DataKey_" + block, data);
-                block++;
-                removeRange(size() - blockSize, size() - 1);
+            if (size() > ramSize) {
+                DataBlock toSave = get(size() - 1);
+                saveData.writeObject(toSave.getName(), toSave);
                 return true;
             }
-            for (IObserver o : observer)
-                if (o != null)
-                    o.call(t);
             return false;
+
         }
 
         @Override
         public DataBlock get(int index) {
-            if (index < size() && index >= 0)
+            index = DataBlock.info.names.size() - 1 - index;
+            if (index < 0)
+                return null;
+
+            if (index < size())
                 return super.get(index);
-            else if (saveData != null && index >= size()) {
-                int i = (index - size()) / blockSize + 1;
-                if (i < block) {
-                    Object[] block = saveData.readObject("DataKey_" + i);
-                    i = (index - size()) % blockSize - 1;
-                    return (Integer) block[i];
-                }
-            }
+
+            String name = DataBlock.getName(index);
+            if (DataBlock.contains(name))
+                return saveData.readObject(name);
             return null;
         }
 
+        public boolean addData(int i) {
+            Element save = new Element(i, Calendar.getInstance());
+            if (get(0).add(save))
+                return false;
+            add(0, new DataBlock(save));
+            return true;
+        }
+
+        public Element getData(int i) {
+            if (i < blockSize) {
+                Element result = get(0).get(i);
+                if (result == null)
+                    i -= blockSize - get(0).index;
+                else return result;
+            }
+            DataBlock block = get(i / blockSize + 1);
+            if (block == null)
+                return null;
+            return block.get(i % blockSize);
+        }
     }
 
     public static class DataInfo implements Serializable {
@@ -220,8 +237,17 @@ public abstract class BreathData {
     public static class DataBlock implements Serializable {
         private Element[] elements;
         private int index = 0;
+        private int id = 0;
         private static DataInfo info;
         private static final String TAG = "BreathDataBlock";
+
+
+        public DataBlock(Element element) {
+            this.id = info.names.size();
+            info.add(getName());
+            elements = new Element[blockSize];
+            elements[index++] = element;
+        }
 
         public static String getName(int id) {
             return TAG + id;
@@ -242,19 +268,34 @@ public abstract class BreathData {
             return true;
         }
 
-        public static class Element {
-            public Integer data;
-            public Calendar date;
+        public Element get(int i) {
+            if (i < index)
+                return elements[i];
+            return null;
+        }
 
-            public Element(Integer data, Calendar date) {
-                this.data = data;
-                this.date = date;
-            }
+        public String getName() {
+            return TAG + id;
+        }
 
-            @Override
-            public String toString() {
-                return data + " at " + date.get(Calendar.HOUR_OF_DAY) + ":" + date.get(Calendar.MINUTE) + ":" + date.get(Calendar.SECOND);
-            }
+        @Override
+        public String toString() {
+            return getName() + ": #" + index;
+        }
+    }
+
+    public static class Element {
+        public Integer data;
+        public Calendar date;
+
+        public Element(Integer data, Calendar date) {
+            this.data = data;
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            return data + " at " + date.get(Calendar.HOUR_OF_DAY) + ":" + date.get(Calendar.MINUTE) + ":" + date.get(Calendar.SECOND);
         }
     }
 
--- Breathy/source/src/main/java/com/apps/philipps/source/SaveData.java
+++ Breathy/source/src/main/java/com/apps/philipps/source/SaveData.java
@@ -99,4 +99,6 @@ public class SaveData<T extends Serializable> {
             e.printStackTrace();
         }
     }
+
+    
 }
