7ef07e85bd0e42106e2ce852c3d99f3ecf6c21e8
jhuebert64p <huebertj@students.uni-marburg.de>
2017-10-22 20:34:38 +0200

    forgot the IStart

---
+++ Breathy/source/src/main/java/com/apps/philipps/source/interfaces/IStart.java
@@ -0,0 +1,26 @@
+package com.apps.philipps.source.interfaces;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Jevgenij Huebert on 27.01.2017. Project Breathy
+ */
+public interface IStart {
+
+    /**
+     * Start Activity. Provide here your states with the <code>AppState</code> Class
+     *
+     * @return true if the Activity started
+     */
+    boolean start();
+
+    /**
+     * Pops the Toast with the given <code>text</code>
+     *
+     * @param text the text to show in the Toast
+     */
+    void message(String text);
+
+}
